542ab32912407ca3403a0802e0eb7afbda7dc3d1
Quentin Herzig <quentin.herzig@resolve.ch>
2026-06-03 16:29:41 +0200

    Fix imports

--- packages/test/src/configs/react.ts
+++ packages/test/src/configs/react.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react"
 import { mergeConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
-import defaultConfig from "./default"
+import defaultConfig from "./default.ts"
 
 export default mergeConfig(defaultConfig, {
   esbuild: { jsx: "automatic", jsxImportSource: "react" },
